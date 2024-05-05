c3cabe6673ca5cc707d9b0ccbc0eb1180af4467d
Anthony deplanque <anthony.deplanque3184@gmail.com>
2024-05-05 16:10:12 +0200

    added merch link

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -10,6 +10,7 @@ import TiktokIcon from "./icons/TiktokIcon";
 import TwitchIcon from "./icons/TwitchIcon";
 import TwitterIcon from "./icons/TwitterIcon";
 import YoutubeIcon from "./icons/YoutubeIcon";
+import MerchIcon from "./icons/MerchIcon";
 
 const LinkList = () => {
 
@@ -30,6 +31,7 @@ const LinkList = () => {
     ["twitter", <TwitterIcon />],
     ["youtube", <YoutubeIcon />],
     ["dons", <DonationIcon />],
+    ["merch", <MerchIcon />],
     ['discord', <DiscordIcon />],
     ['instant gaming', <GamepadIcon />]
   ])
